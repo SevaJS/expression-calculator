75d5fa3932ac00c16252823118c593cd6568b980
Semion <86849135+SevaJS@users.noreply.github.com>
2021-08-30 21:44:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let openBracket = 0;
+    let closeBracket = 0;
+
+    if (expr.indexOf('/ 0') + 1) throw new Error('TypeError: Division by zero.');
+
+    for(let i = 0; i < expr.length; i++) {
+        if (expr[i] === '(') openBracket++;
+        if (expr[i] === ')') closeBracket++;
+    }
+    if (openBracket !== closeBracket) throw new Error('ExpressionError: Brackets must be paired.');
+    return new Function (`return ${expr}`)();
 }
 
 module.exports = {
